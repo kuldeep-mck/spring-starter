10a10ab7c257b5ba5228833087e63d36669e0ffb
Kuldeep Singh <kuldeep_singh-bebr@mckinsey.com>
2024-04-15 21:41:08 +0530

    add: JPA repository & hibernate migration

--- src/main/java/com/initiative/banking/entities/Student.java
+++ src/main/java/com/initiative/banking/entities/Student.java
@@ -1,22 +1,19 @@
 package com.initiative.banking.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@RedisHash("Student")
 public class Student implements Serializable {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String name;
     private String grade;
     private String contactNo;
-
 }
--- src/main/java/com/initiative/banking/repositories/StudentRepository.java
+++ src/main/java/com/initiative/banking/repositories/StudentRepository.java
@@ -1,10 +1,10 @@
 package com.initiative.banking.repositories;
 
 import com.initiative.banking.entities.Student;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends CrudRepository<Student, String> {
+public interface StudentRepository extends JpaRepository<Student, String> {
 
 }
